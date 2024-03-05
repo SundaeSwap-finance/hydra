bffb503898c6a0866fef5b33e56183318f49c662
card <cardenaso11@users.noreply.github.com>
2024-03-05 11:55:54 -0500

    newline in udp output

--- hydra-node/src/Hydra/Events/UDP.hs
+++ hydra-node/src/Hydra/Events/UDP.hs
@@ -7,18 +7,9 @@ import Hydra.Events (EventSink (..), HasEventId (..))
 import Network.Socket (HostName, ServiceName)
 import Network.UDP (clientSocket, send)
 
--- To build a sink:
---
-
 exampleUDPSink :: (HasEventId e, ToJSON e) => HostName -> ServiceName -> IO (EventSink e IO)
 exampleUDPSink addr port = do
   socket <- clientSocket addr port False
   pure $ EventSink $ \e -> do
-    send socket (toStrict $ encode e)
+    send socket (toStrict $ encode e <> "\n")
     putStrLn $ "Sending event " <> show (getEventId e) <> " to UDP"
-
--- To build a source:
-
--- TODO: Do we want this? I'd assume probably not for a first demo
-exampleUDPSource :: a
-exampleUDPSource = undefined
