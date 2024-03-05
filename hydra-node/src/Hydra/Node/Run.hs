919a6c2ea7edff16373a616134c5afd1280ef8c8
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-03-05 17:34:12 +0100

    Re-use socket in UDP sink

--- hydra-node/src/Hydra/Node/Run.hs
+++ hydra-node/src/Hydra/Node/Run.hs
@@ -82,9 +82,10 @@ run opts = do
         (eventSource, filePersistenceSink) <- eventPairFromPersistenceIncremental incPersistence
         -- NOTE: Add any custom sink setup code here
         -- customSink <- createCustomSink
+        udpSink <- exampleUDPSink "0.0.0.0" "3000"
         let eventSinks =
               [ filePersistenceSink
-              , exampleUDPSink "localhost" "3000"
+              , udpSink
               -- NOTE: Add any custom sinks here
               -- , customSink
               ]
